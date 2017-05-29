9a092613ccad6159ab2ea9657a6a448b580b7d59
REDMOND\v-rugali <v-rugali@microsoft.com>
2017-05-29 08:41:40 -0700

    Titanic problem. Random Forest. Accuracy 0.8548

--- Titanic/titanic2.py
+++ Titanic/titanic2.py
@@ -10,21 +10,34 @@ train = pd.read_csv('train.csv')
 test = pd.read_csv('test.csv')
 combine = pd.concat([train.drop("Survived", 1), test], ignore_index = True)
 
-# Encoding categorical data
+# Sex
 from sklearn.preprocessing import LabelEncoder
 labelencoder = LabelEncoder()
 combine["Sex"] = labelencoder.fit_transform(combine["Sex"])
 
-# Taking care of missing data
-from sklearn.preprocessing import Imputer
-imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
-imputer = imputer.fit(combine[["Age"]])
-combine["Age"] = imputer.transform(combine[["Age"]]).ravel()
+# Fare
 combine.loc[combine["Fare"].isnull(), "Fare"] = combine.loc[combine["Pclass"] == 3, "Fare"].median()
 
+# Title
+combine['Title'] = combine['Name'].str.split(", ", expand=True)[1].str.split(".", expand=True)[0]
+# combine.groupby("Title")["Name"].count().sort_values(ascending = False)
+combine['IsMister'] = combine['Title'] == "Mr"
+combine['IsMiss'] = combine['Title'] == "Miss"
+combine['IsMrs'] = combine['Title'] == "Mrs"
+combine['IsMaster'] = combine['Title'] == "Master"
 
+# Age
+combine.loc[combine["Age"].isnull(), "Age"] = combine["Age"].dropna().median()
+combine["IsChild"] = combine["Age"] <= 10
+combine["IsYoung"] = (combine["Age"] >= 18) & (combine["Age"] <= 40)
+combine["IsYoung_M"] = combine["IsYoung"] & (combine["Sex"] == 1)
+combine["IsYoung_F"] = combine["IsYoung"] & (combine["Sex"] == 0)
+
+# Family
 combine["IsAlone"] = combine["SibSp"] + combine["Parch"] == 0
-combine["IsBigFamily"] = combine["SibSp"] + combine["Parch"] >= 5
+combine["IsBigFamily"] = (combine["SibSp"] > 2) | (combine["Parch"] > 3)
+
+
 
 # Embarked
 combine.loc[combine["Embarked"].isnull(), "Embarked"] = "C"
@@ -34,12 +47,13 @@ combine["Embarked"] = combine["Embarked"].astype("int")
 combine["Embarked_C"] = combine["Embarked"] == 0
 combine["Embarked_Q"] = combine["Embarked"] == 1
 combine["Embarked_S"] = combine["Embarked"] == 2
-       
-cols = ["Sex", "Pclass", "Age", "IsAlone", "IsBigFamily", "Embarked_C", "Embarked_Q", "Embarked_S", "Fare", "Parch", "SibSp"]
+
+cols = ["IsYoung", "Sex", "Pclass", "Age", "IsYoung_M", "IsMister", "IsMiss", "IsMrs", "IsMaster",\
+        "IsAlone", "IsBigFamily", "IsYoung_F", "Embarked_C", "Embarked_Q", "Embarked_S", "IsChild", "Fare", "Parch", "SibSp"]
 X = combine.loc[combine["PassengerId"] <= 891, cols]
 X_submission = combine.loc[combine["PassengerId"] > 891, cols]
 y = train.loc[:, "Survived"]
-            
+
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
@@ -63,64 +77,6 @@ from sklearn.metrics import accuracy_score
 s = accuracy_score(y_test, y_pred)
 
 scores.append(tuple(("Logistic Regression", s)))
-
-# Fitting KNN classifier to the Training set
-from sklearn.neighbors import KNeighborsClassifier
-classifier = KNeighborsClassifier(n_neighbors = 5, metric = 'minkowski', p = 2)
-classifier.fit(X_train, y_train)
-
-# Predicting the Test set results
-y_pred = classifier.predict(X_test)
-
-# Making the Confusion Matrix
-from sklearn.metrics import confusion_matrix
-cm = confusion_matrix(y_test, y_pred)
-
-# Accuracy
-from sklearn.metrics import accuracy_score
-s = accuracy_score(y_test, y_pred)
-
-scores.append(tuple(("K-Nearest Neighbors", s)))
-
-
-# Fitting SVM classifier to the Training set
-from sklearn.svm import SVC
-classifier = SVC(kernel = 'linear', random_state = 0)
-classifier.fit(X_train, y_train)
-
-# Predicting the Test set results
-y_pred = classifier.predict(X_test)
-
-# Making the Confusion Matrix
-from sklearn.metrics import confusion_matrix
-cm = confusion_matrix(y_test, y_pred)
-
-# Accuracy
-from sklearn.metrics import accuracy_score
-s = accuracy_score(y_test, y_pred)
-
-scores.append(tuple(("SVM linear", s)))
-
-
-
-# Fitting classifier to the Training set
-from sklearn.svm import SVC
-classifier = SVC(kernel = 'rbf', random_state = 0)
-classifier.fit(X_train, y_train)
-
-# Predicting the Test set results
-y_pred = classifier.predict(X_test)
-
-# Making the Confusion Matrix
-from sklearn.metrics import confusion_matrix
-cm = confusion_matrix(y_test, y_pred)
-
-# Accuracy
-from sklearn.metrics import accuracy_score
-s = accuracy_score(y_test, y_pred)
-
-scores.append(tuple(("SVM rbf", s)))
-
 
 
 # Fitting GaussianNB classifier to the Training set
@@ -207,8 +163,18 @@ xdiff = max(val) - min(val)
 axes.set_xlim([min(val) - xdiff*.1, max(val) + xdiff*.1])
 plt.show()
 
+# Write scores to the file
+from datetime import datetime
+newScore = pd.DataFrame()
+newScore.loc[0, "Time"] = str(datetime.now())
+newScore.loc[0, "Score"] = scores[-1][1]
+sc = pd.read_csv("scores.csv")
+sc = sc.append(newScore)
+sc.to_csv("scores.csv", index = False)
+
 # Prepare the submission
 submission = pd.DataFrame()
 submission["PassengerId"] = test.loc[:, "PassengerId"]
 submission["Survived"] = classifier.predict(X_submission)
 submission.to_csv("submission.csv", index = False)
+
